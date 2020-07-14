790a5911a8f081b29e093c703873c9e042f01d22
Aayush Soni <Aayushsoni357@gmail.com>
2020-07-14 15:26:39 +0530

    Updates

---
+++ precache-manifest.4b96889c9240b82a21dc888c2d891f70.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae0c82ef5361336fe98c35783511ba1",
+    "url": "/gym-adda/index.html"
+  },
+  {
+    "revision": "35ef7ea4b2a6dc45aeb3",
+    "url": "/gym-adda/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7bd83f42d2099dbccd87",
+    "url": "/gym-adda/static/css/main.88e5a629.chunk.css"
+  },
+  {
+    "revision": "35ef7ea4b2a6dc45aeb3",
+    "url": "/gym-adda/static/js/2.31f4fe58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gym-adda/static/js/2.31f4fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd83f42d2099dbccd87",
+    "url": "/gym-adda/static/js/main.7594b181.chunk.js"
+  },
+  {
+    "revision": "5c63058dd4eadf8d03e2",
+    "url": "/gym-adda/static/js/runtime-main.f3dc46b3.js"
+  },
+  {
+    "revision": "16c0d64eb55dbdbb3e914da61eb81d4c",
+    "url": "/gym-adda/static/media/Logo.16c0d64e.svg"
+  },
+  {
+    "revision": "aadd07160dffa205cb6a4eb9800c572d",
+    "url": "/gym-adda/static/media/background.aadd0716.png"
+  },
+  {
+    "revision": "abd9ba68cbb44138a78bbe63b4abbfd5",
+    "url": "/gym-adda/static/media/background.abd9ba68.png"
+  }
+]);
